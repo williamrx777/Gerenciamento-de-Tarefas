86963f5177f9294442407d4c3ca8a3b220153529
william rodrigues <williamr723@gmail.com>
2023-08-25 14:01:36 -0300

    start sistema de gerenciamento de sistemas

---
+++ gerenciamentosTarefas/src/main/java/com/gerenciamento/tarefa/GerenciamentosTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciamento.tarefa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentosTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentosTarefasApplication.class, args);
+	}
+
+}
